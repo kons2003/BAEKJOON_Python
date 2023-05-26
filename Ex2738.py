2a08260884c1db225ef5c95c2146fc70424e029a
kons2003 <mj030313@naver.com>
2023-05-26 22:43:20 +0900

    Rook, 행렬 덧셈

---
+++ Ex2738.py
@@ -0,0 +1,16 @@
+# 행렬 덧셈
+a , b = [], []
+n, m = map(int, input().split())
+
+for i in range(n):
+  i = list(map(int, input().split()))
+  a.append(i)
+
+for i in range(n):
+  i = list(map(int, input().split()))
+  b.append(i)
+
+for i in range(n):
+  for j in range(m):
+    print(a[i][j] + b[i][j], end=' ')
+  print()
