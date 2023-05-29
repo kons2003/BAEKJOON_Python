c8a9afb24eff039fb5bc05e54d10a1c74dbbe60f
kons2003 <mj030313@naver.com>
2023-05-30 05:03:07 +0900

    와이버스 부릉부릉

---
+++ Ex14645.py
@@ -0,0 +1,11 @@
+# 와이버스 부릉부릉
+print("비와이")
+
+# import sys
+# input = sys.stdin.readline
+
+# n, k = map(int, input().split())
+# for i in range(n):
+#     a, b = map(int, input().split())
+
+# print("비와이")
