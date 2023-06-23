a7984ef55eb37749aaba59bb068567efd7e3e620
kons2003 <mj030313@naver.com>
2023-06-23 11:20:51 +0900

    럭비 클럽

--- Ex2083.py
+++
@@ -1,9 +0,0 @@
-# 럭비 클럽
-while 1:
-    name, age, weight = input().split()
-    if name == '#':
-        break
-    if int(age) > 17 or int(weight) >= 80:
-        print(f'{name} Senior')
-    else:
-        print(f'{name} Junior')
