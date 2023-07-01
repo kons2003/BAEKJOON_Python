2d856e1f61fb844914955d1a28f86cc79fa9cf40
kons2003 <mj030313@naver.com>
2023-07-01 16:30:24 +0900

    Gnome Sequencing

---
+++ Bronze IV [4]/Ex4589.py
@@ -0,0 +1,9 @@
+# Gnome Sequencing
+n = int(input())
+print("Gnomes:")
+for i in range(n):
+    a, b, c = map(int, input().split())
+    if a<=b and b<=c or a>=b and b>=c:
+        print("Ordered")
+    else:
+        print("Unordered")
