da03806fbdd0478499bbf1e0ac206f9dc88c1a74
kons2003 <mj030313@naver.com>
2023-07-17 11:30:54 +0900

    Speed fines are not fine!

---
+++ Bronze IV [4]/Ex6763.py
@@ -0,0 +1,12 @@
+# Speed fines are not fine!
+a = int(input())
+b = int(input())
+c = b-a
+if 1<=c and c<=20:
+    print("You are speeding and your fine is $100.")
+elif 21<=c and c<=30:
+    print("You are speeding and your fine is $270.")
+elif 31<=c:
+    print("You are speeding and your fine is $500.")
+else:
+    print("Congratulations, you are within the speed limit!")
