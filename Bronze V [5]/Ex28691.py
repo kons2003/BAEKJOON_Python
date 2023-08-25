02ab508795b41b3b4ee14173f0fcee9e034c7de6
kons2003 <mj030313@naver.com>
2023-08-26 07:47:39 +0900

    정보보호학부 동아리 소개

---
+++ Bronze V [5]/Ex28691.py
@@ -0,0 +1,12 @@
+# 정보보호학부 동아리 소개
+c = input()
+if c == 'M':
+    print("MatKor")
+elif c == 'W':
+    print("WiCys")
+elif c == 'C':
+    print("CyKor")
+elif c == 'A':
+    print("AlKor")
+elif c == '$':
+    print("$clear")
